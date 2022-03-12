cf58904192d1fe379059fd12890e0a68aaf593c3
Juan Carrillo <inetshell@gmail.com>
2022-03-12 01:09:21 -0600

    Fixed issues in PubSub module

--- pubsub.tf
+++ pubsub.tf
@@ -7,6 +7,8 @@ module "pubsub_topics" {
   for_each   = var.pubsub_topics
   source     = "./modules/pubsub_topics/"
   project_id = var.project_id
+  topic = each.key
+  create_default_subscription = lookup(each.value, "create_default_subscription", true )
 }
 
 output "pubsub_topics" {
