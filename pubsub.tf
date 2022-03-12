9ade0ef318b25a4e642c703cb9c2a1d828cb0529
Juan Carrillo <inetshell@gmail.com>
2022-03-12 17:55:04 -0600

    Fixed format

--- pubsub.tf
+++ pubsub.tf
@@ -1,14 +1,14 @@
 variable "pubsub_topics" {
-  type = any
+  type    = any
   default = {}
 }
 
 module "pubsub_topics" {
-  for_each   = var.pubsub_topics
-  source     = "./modules/pubsub_topics/"
-  project_id = var.project_id
-  topic = each.key
-  create_default_subscription = lookup(each.value, "create_default_subscription", true )
+  for_each                    = var.pubsub_topics
+  source                      = "./modules/pubsub_topics/"
+  project_id                  = var.project_id
+  topic                       = each.key
+  create_default_subscription = lookup(each.value, "create_default_subscription", true)
 }
 
 output "pubsub_topics" {
