e3d983018b47c6a87f990809ae628e1757753adb
Juan Carrillo <inetshell@gmail.com>
2022-03-18 13:29:26 -0600

    Added service account module

---
+++ service_accounts.tf
@@ -0,0 +1,19 @@
+variable "service_accounts" {
+  type = any
+}
+
+module "service_accounts" {
+  for_each      = var.service_accounts
+  source        = "terraform-google-modules/service-accounts/google"
+  version       = "4.1.1"
+  project_id    = var.project_id
+  prefix        = lookup(each.value, "prefix", "")
+  names         = [each.key]
+  project_roles = lookup(each.value, "project_roles", [])
+  generate_keys = lookup(each.value, "generate_keys", false)
+}
+
+output "service_accounts" {
+  value     = module.service_accounts
+  sensitive = true
+}
