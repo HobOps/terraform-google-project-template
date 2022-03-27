7f262b14dfa75c4b4040875aac60acce8459a608
Juan Carrillo <inetshell@gmail.com>
2022-03-27 17:07:27 -0600

    Added Cloud KMS module

---
+++ kms.tf
@@ -0,0 +1,30 @@
+variable "kms" {
+  type    = any
+  default = {}
+}
+
+module "kms" {
+  for_each = var.kms
+  source   = "terraform-google-modules/kms/google"
+  version  = "2.1.0"
+
+  project_id           = var.project_id
+  keyring              = each.key
+  location             = lookup(each.value, "location", "global")
+  prevent_destroy      = lookup(each.value, "prevent_destroy", true)
+  keys                 = lookup(each.value, "keys", [])
+  labels               = lookup(each.value, "labels", {})
+  set_decrypters_for   = lookup(each.value, "set_decrypters_for", [])
+  set_encrypters_for   = lookup(each.value, "set_encrypters_for", [])
+  set_owners_for       = lookup(each.value, "set_owners_for", [])
+  decrypters           = lookup(each.value, "decrypters", [])
+  encrypters           = lookup(each.value, "encrypters", [])
+  owners               = lookup(each.value, "owners", [])
+  key_algorithm        = lookup(each.value, "key_algorithm", "GOOGLE_SYMMETRIC_ENCRYPTION")
+  key_protection_level = lookup(each.value, "key_protection_level", "SOFTWARE")
+  key_rotation_period  = lookup(each.value, "key_rotation_period", "100000s")
+}
+
+output "kms" {
+  value = module.kms
+}
--- service_accounts.tf
+++ service_accounts.tf
@@ -1,5 +1,5 @@
 variable "service_accounts" {
-  type = any
+  type    = any
   default = {}
 }
 
