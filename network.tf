0b11b6657df3b9a8b0c01e32dc7873bba50e79c1
Juan Carrillo <inetshell@gmail.com>
2022-09-06 23:10:01 -0500

    Add support for internal addresses

--- network.tf
+++ network.tf
@@ -33,7 +33,7 @@ module "addresses" {
   ip_version   = lookup(each.value, "ip_version", "IPV4")
   global       = lookup(each.value, "global", false)
   network_tier = lookup(each.value, "network_tier", "PREMIUM")
-  addresses    = lookup(each.value, "addresses", null)
+  addresses    = lookup(each.value, "addresses", [])
   subnetwork   = lookup(each.value, "subnetwork", null)
 }
 
